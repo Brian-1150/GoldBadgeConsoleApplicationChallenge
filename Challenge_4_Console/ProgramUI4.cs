753df9d8a82c5ae2b6cd88ea188fb8a7c7f3ac63
Brian Campassi <bpcampassi@gmail.com>
2020-12-12 20:49:28 -0500

    UI

--- Challenge_4_Console/ProgramUI4.cs
+++ Challenge_4_Console/ProgramUI4.cs
@@ -5,11 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Challenge_4_Console {
-    class Program {
-        static void Main(string[] args) {
-
-            ProgramUI4 prgrm = new ProgramUI4();
-            prgrm.Run();
-        }
+    class Class1 {
     }
 }
