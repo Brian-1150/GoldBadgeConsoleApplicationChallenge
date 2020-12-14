8b99d26f2bd0dcbcdf3298cca0556e2b436d6647
Brian Campassi <bpcampassi@gmail.com>
2020-12-13 23:02:23 -0500

    Add and detail methods

--- Challenge_4_Console/ProgramUI4.cs
+++ Challenge_4_Console/ProgramUI4.cs
@@ -19,23 +19,25 @@ namespace Challenge_4_Console {
         public void Menu() {
             bool exit = false;
             while (exit == false) {
-                Console.WriteLine("   ****Main Menu****\n" +
+                Console.WriteLine("     ****Main Menu****\n\n\n" +
                                   "Please make a selection\n" +
-                                  "1.  View Outings\n" +
-                                  "2.  View Outings by Category\n" +
-                                  "3.  Add a New Outing\n" +
-                                  "4.  See Costs for Outings\n" +
-                                  "5.  Edit Detials for an Outing\n" +
-                                  "6.  Exit\n");
+                                  "1.  View Outing Details\n" +
+                                  "2.  Add a New Outing\n" +
+                                  "3.  Edit Detials for an Outing\n" +
+                                  "4.  Exit\n");
+
                 string input = Console.ReadLine();
                 switch (input) {
                     case "1": //View
-                        ViewAllOutings();
-                        break;
-                    case "2": //View by type
                         ViewOutingsByDate();
                         break;
-                    case "6": //Exit
+                    case "2": //View by type
+                        AddNewOuting();
+                        break;
+                    case "3": //Edit
+                        EditOuting();
+                        break;
+                    case "4": //Exit
                         Console.WriteLine("Have a great day!");
                         exit = true;
                         break;
@@ -43,31 +45,32 @@ namespace Challenge_4_Console {
                         Console.WriteLine("Please make a valid selection");
                         break;
                 }
+                Console.WriteLine("Please press any key to continue...");
+                Console.ReadKey();
+                Console.Clear();
             }
         }
 
-        public void ViewAllOutings() {
-            var list = _repo.ListOfOutings();
-            Console.WriteLine("\t\t\t\tCOST PER");
-            Console.WriteLine("DATE\t\tEVENT TYPE\t PERSON\t\tTOTAL COST\n\n");
-            foreach (var outing in list) {
-                if (outing.TypeOfOuting == OutingType.Theme_Park) {
-                    Console.WriteLine($"{outing.DateOfEvent.ToShortDateString()}\t{outing.TypeOfOuting}\t" +
-                        $"${outing.CostPerPerson.ToString("#,#")}\t\t${outing.EventCost.ToString("#,#")}");
-                }
-                else {
-                    Console.WriteLine($"{outing.DateOfEvent.ToShortDateString()}\t{outing.TypeOfOuting}\t\t" +
-                        $"${outing.CostPerPerson.ToString("#,#")}\t\t${outing.EventCost.ToString("#,#")}");
-                }
-            }
-            Console.WriteLine("\nWould you like to refine resuts by category? y/n");
-            if (!YesOrNO(Console.ReadLine())) { return; }
+        //public void ViewAllOutings() {
+        //    var list = _repo.ListOfOutings();
+        //    Console.WriteLine("\t\t\t\tCOST PER");
+        //    Console.WriteLine("DATE\t\tEVENT TYPE\t PERSON\t\tTOTAL COST\n\n");
+        //    foreach (var outing in list) {
+        //        if (outing.TypeOfOuting == OutingType.Theme_Park) {
+        //            Console.WriteLine($"{outing.DateOfEvent.ToShortDateString()}\t{outing.TypeOfOuting}\t" +
+        //                $"${outing.CostPerPerson.ToString("#,#")}\t\t${outing.EventCost.ToString("#,#")}");
+        //        }
+        //        else {
+        //            Console.WriteLine($"{outing.DateOfEvent.ToShortDateString()}\t{outing.TypeOfOuting}\t\t" +
+        //                $"${outing.CostPerPerson.ToString("#,#")}\t\t${outing.EventCost.ToString("#,#")}");
+        //        }
+        //    }
 
-            
-
-        }
-
+        //}
+        //View
         public void ViewOutingsByDate() {
+            Console.Clear();
+            double total = 0;
             var list = ListOfOutingsByDate();
             Console.WriteLine("\t\t\t\tCOST PER");
             Console.WriteLine("DATE\t\tEVENT TYPE\t PERSON\t\tTOTAL COST\n\n");
@@ -75,23 +78,167 @@ namespace Challenge_4_Console {
                 if (outing.TypeOfOuting == OutingType.Theme_Park) {
                     Console.WriteLine($"{outing.DateOfEvent.ToShortDateString()}\t{outing.TypeOfOuting}\t" +
                         $"${outing.CostPerPerson.ToString("#,#")}\t\t${outing.EventCost.ToString("#,#")}");
+                    total += outing.EventCost;
                 }
                 else {
                     Console.WriteLine($"{outing.DateOfEvent.ToShortDateString()}\t{outing.TypeOfOuting}\t\t" +
                         $"${outing.CostPerPerson.ToString("#,#")}\t\t${outing.EventCost.ToString("#,#")}");
+                    total += outing.EventCost;
                 }
             }
+            Console.WriteLine($"Total combined cost for all outings this year:  ${total}\n");
+            Console.WriteLine("\nWould you like to refine resuts by category? y/n");
+            if (!YesOrNO(Console.ReadLine())) { return; }
+            OutingSubMenu();
+        }
+        //View Submenu
+        public void OutingSubMenu() {
+            Console.Clear();
+            bool exit = false;
+            while (exit == false) {
+                Console.WriteLine("Details by Category:\n\n" +
+                "1.  Bowling\n" +
+                "2.  Golf\n" +
+                "3.  Theme Park\n" +
+                "4.  Concert\n" +
+                "5.  Exit");
+                string input = Console.ReadLine();
+                double total = 0;
+                switch (input) {
+                    case "1": //Bowling
+                        var bowlingList = ListByCategory("1");
+                        Console.WriteLine("\t\t\t\tCOST PER");
+                        Console.WriteLine("DATE\t\tEVENT TYPE\t PERSON\t\tATTENDANCE\tTOTAL COST\n\n");
+                        foreach (var outing in bowlingList) {
+
+                            Console.WriteLine($"{outing.DateOfEvent.ToShortDateString()}\t{outing.TypeOfOuting}\t\t" +
+                        $"${outing.CostPerPerson.ToString("#,#")}\t\t{outing.Attendance}\t\t${outing.EventCost.ToString("#,#")}");
+                            total += outing.EventCost;
+                        }
+                        Console.WriteLine($"\nTotal cost for all bowling events this year:\t\t\t${total.ToString("#,#")}\n");
+                        break;
+                    case "2": //Golf
+                        var golfList = ListByCategory("2");
+                        Console.WriteLine("\t\t\t\tCOST PER");
+                        Console.WriteLine("DATE\t\tEVENT TYPE\t PERSON\t\tATTENDANCE\tTOTAL COST\n\n");
+                        foreach (var outing in golfList) {
+
+                            Console.WriteLine($"{outing.DateOfEvent.ToShortDateString()}\t{outing.TypeOfOuting}\t\t" +
+                        $"${outing.CostPerPerson.ToString("#,#")}\t\t{outing.Attendance}\t\t${outing.EventCost.ToString("#,#")}");
+                            total += outing.EventCost;
+                        }
+                        Console.WriteLine($"\nTotal cost for all golf events this year: \t\t        ${total.ToString("#,#")}\n");
+                        break;
+                    case "3": //Theme Park
+                        var themeParkList = ListByCategory("3");
+                        Console.WriteLine("\t\t\t\tCOST PER");
+                        Console.WriteLine("DATE\t\tEVENT TYPE\t PERSON\t\tATTENDANCE\tTOTAL COST\n\n");
+                        foreach (var outing in themeParkList) {
+
+                            Console.WriteLine($"{outing.DateOfEvent.ToShortDateString()}\t{outing.TypeOfOuting}\t" +
+                        $"${outing.CostPerPerson.ToString("#,#")}\t\t{outing.Attendance}\t\t${outing.EventCost.ToString("#,#")}");
+                            total += outing.EventCost;
+                        }
+                        Console.WriteLine($"\nTotal cost for all bowling events this year: \t\t        ${total.ToString("#,#")}\n");
+                        break;
+                    case "4": //Concert
+                        var concertList = ListByCategory("4");
+                        Console.WriteLine("\t\t\t\tCOST PER");
+                        Console.WriteLine("DATE\t\tEVENT TYPE\t PERSON\t\tATTENDANCE\tTOTAL COST\n\n");
+                        foreach (var outing in concertList) {
+
+                            Console.WriteLine($"{outing.DateOfEvent.ToShortDateString()}\t{outing.TypeOfOuting}\t\t" +
+                         $" ${outing.CostPerPerson.ToString("#,#")}\t\t{outing.Attendance}\t\t${outing.EventCost.ToString("#,#")}");
+                            total += outing.EventCost;
+                        }
+                        Console.WriteLine($"\nTotal cost for all bowling events this year: \t\t        ${total.ToString("#,#")}\n");
+                        break;
+                    case "5": //Return
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Please make a valid selection");
+                        break;
+                }
+                Console.WriteLine("Please press any key to continue...");
+                Console.ReadKey();
+
+                Console.Clear();
+            }
+        }
+        //Add
+        public void AddNewOuting() {
+            Console.Clear();
+            Console.WriteLine("What type of outing would you like to add?");
+            Console.WriteLine("1.  Theme Park\n2.  Bowling\n3.  Concert\n4.  Golf");
+            int type = TryParse(Console.ReadLine());
+            Console.WriteLine("When did this event take place?  Please enter the four digit year. ex(2020)");
+            int year = TryParse(Console.ReadLine());
+            Console.WriteLine("Now enter the two digit month.  ex (01 for January)");
+            int month = TryParse(Console.ReadLine());
+            Console.WriteLine("Now enter the two digit day. ex (11)");
+            int day = TryParse(Console.ReadLine());
+            Console.WriteLine("How many people attended the event?");
+            int attendance = TryParse(Console.ReadLine());
+            Console.WriteLine("What was the cost per person?");
+            double cost = TryParseDouble(Console.ReadLine());//rounding up??????
+            var newOuting = new Outings(attendance, new DateTime(year, month, day), cost, (OutingType)type);
+            _repo.AddListingToList(newOuting);
+
         }
 
+        //Edit
+        public void EditOuting() {
 
+        }
 
+        //Helper Methods
+        //View by category
+        public List<Outings> ListByCategory(string choice) {
+            var tempList = new List<Outings>();
+            var list = _repo.ListOfOutings();
+            switch (choice) {
+                case "1":
+                    foreach (var outing in list) {
+                        if (outing.TypeOfOuting == OutingType.Bowling) {
+                            tempList.Add(outing);
+                        }
+                    }
+                    break;
+                case "2":
+                    foreach (var outing in list) {
+                        if (outing.TypeOfOuting == OutingType.Golf) {
+                            tempList.Add(outing);
+                        }
+                    }
+                    break;
+                case "3":
+                    foreach (var outing in list) {
+                        if (outing.TypeOfOuting == OutingType.Theme_Park) {
+                            tempList.Add(outing);
+                        }
+                    }
+                    break;
+                case "4":
+                    foreach (var outing in list) {
+                        if (outing.TypeOfOuting == OutingType.Concert) {
+                            tempList.Add(outing);
+                        }
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Sorry there was problem.  Please try again!");
+                    break;
+            }
+            return tempList;
+        }
 
-        //Helper Method
         public List<Outings> ListOfOutingsByDate() {
             var list = _repo.ListOfOutings();
             list.Sort((a, b) => a.DateOfEvent.CompareTo(b.DateOfEvent));
             return list;
-            }
+        }
+
         //Parse
         public int TryParse(string number) {
 
@@ -100,6 +247,15 @@ namespace Challenge_4_Console {
                 Console.WriteLine("Please enter a valid number");
                 number = Console.ReadLine();
                 int.TryParse(number, out k);
+            }
+            return k;
+        }
+        public double TryParseDouble(string number) {
+            double.TryParse(number, out double k);
+            while (k <= 0) {
+                Console.WriteLine("Please enter a valid number");
+                number = Console.ReadLine();
+                double.TryParse(number, out k);
             }
             return k;
         }
@@ -133,5 +289,6 @@ namespace Challenge_4_Console {
             Outings _seedOuting5 = new Outings(25, new DateTime(2020, 2, 14), 25, OutingType.Bowling);
             Outings _seedOuting6 = new Outings(5, new DateTime(2020, 5, 5), 150, (OutingType)1);
             _repo.AddListingToList(_seedOuting, _seedOuting6, _seedOuting5, _seedOuting4, _seedOuting3, _seedOuting2);
-    } }
+        }
+    }
 }
