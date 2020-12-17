da71f39ad04cab7d3fc2f24e335277d37a139cff
Brian Campassi <bpcampassi@gmail.com>
2020-12-17 00:10:27 -0500

    Challenge6 POCO

---
+++ Challenge_6_Repository/Vehicle.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_6_Repository {
+    public class Vehicle {
+
+        public enum VehicleType {
+            Sedan = 1,
+            Van,
+            Truck,
+            Coupe,
+            Other
+
+        }
+        public string Make_Model { get; set; }
+        public double RetailCost { get; set; }
+        public VehicleType TypeOfVehicle { get; set; }
+
+        public Vehicle() { }
+        public Vehicle(string makeModel, double retailCost, VehicleType typeOfVehicle) {
+            Make_Model = makeModel;
+            RetailCost = retailCost;
+            TypeOfVehicle = typeOfVehicle;
+        }
+    }
+    public class GasVehicle : Vehicle {
+        public double FuelEconomy { get; set; }
+
+        public GasVehicle() { }
+        public GasVehicle(double fuelEconomy, string makeModel, double retailCost, VehicleType typeOfVehicle) 
+            : base (makeModel, retailCost, typeOfVehicle) {
+            FuelEconomy = fuelEconomy;
+        }
+        
+    }
+    public class ElectricVehcile : Vehicle {
+        public double PowerRating { get; set; }
+
+        public ElectricVehcile() { }
+        public ElectricVehcile(double powerRating, string makeModel, double retailCost, VehicleType typeOfVehicle)
+            : base(makeModel, retailCost, typeOfVehicle) {
+            PowerRating = powerRating;
+        }
+    }
+    public class HybridVehcile : GasVehicle  {
+        public double PowerRating { get; set; }
+
+        public HybridVehcile() { }
+        public HybridVehcile(double powerRating, double fuelEconomy, string makeModel, double retailCost, VehicleType typeOfVehicle)
+            : base(fuelEconomy, makeModel, retailCost, typeOfVehicle) {
+            PowerRating = powerRating;
+        }
+    }
+}
---
+++ Challenge_6_Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Challenge_6_Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Challenge_6_Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f0118a02-ff14-4b42-b09d-fb996b474b68")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
