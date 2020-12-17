55cea33470c824bb85f7f0deb484ae74105f13fc
Brian Campassi <bpcampassi@gmail.com>
2020-12-17 00:28:16 -0500

    Challenge8 POCO

---
+++ Challenge_8_Console/ProgramUI8.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_8_Console {
+    class ProgramUI8 {
+    }
+}
---
+++ Challenge_8_Repository/Driver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Challenge_5_Repository;
+using Challenge_6_Repository;
+
+namespace Challenge_8_Repository {
+    public class Driver : Customer {
+        public Vehicle Type { get; set; }
+        public bool VehicleTypDiscount { get; set; } 
+        public int Points { get; set; } = 5;
+        public int MonthsWithoutInfraction { get; set; }
+    }
+}
